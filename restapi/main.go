adb051ef112addf01bf61c43409f6672a0154732
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-18 09:41:11 +0330

    endpoints and route handlers initialized

--- restapi/main.go
+++ restapi/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	_ "encoding/json"
-	_ "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 	_ "log"
 	_ "math/rand"
 	_ "net/http"
@@ -10,5 +10,13 @@ import (
 )
 
 func main() {
+	// Init router
+	r := mux.NewRouter()
 
+	// Route handlers / Endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 }
