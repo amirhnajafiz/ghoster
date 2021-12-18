015c4ec68bd1669309669be2fdeb247eb9634721
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-18 14:46:12 +0330

    gorm connection to database is set

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
+	gorm.io/driver/mysql v1.2.1 // indirect
 	gorm.io/gorm v1.22.4 // indirect
 )
--- restapi/config/database/main.go
+++ restapi/config/database/main.go
@@ -1,6 +1,20 @@
 package database
 
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
 // Connect : creates a connection to our database
 func Connect() {
+	dsn := "root:Amir2222@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
+	if err != nil {
+		panic(err.Error())
+	}
+
+	DB = db
 }
