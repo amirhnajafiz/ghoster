438b79e27fef7de07adeecc86377b20a84da1afd
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-18 09:46:11 +0330

    struct models are initialized

--- restapi/main.go
+++ restapi/main.go
@@ -10,6 +10,20 @@ import (
 	_ "strconv"
 )
 
+// Book struct (Model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// Author struct
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
 func main() {
 	// Init router
 	r := mux.NewRouter()
