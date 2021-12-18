ca7297f212c7568149da89d93a8be8fe402ea3ac
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-18 10:15:37 +0330

    delete book method implemented properly

--- restapi/main.go
+++ restapi/main.go
@@ -66,8 +66,19 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// Removing a book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
 
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+
+	_ = json.NewEncoder(w).Encode(books)
 }
 
 func main() {
