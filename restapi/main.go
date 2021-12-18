a1c788c3c09f3816b72d6454ce1883d820ae9187
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-18 10:18:41 +0330

    updating a book bug in id fixed

--- restapi/main.go
+++ restapi/main.go
@@ -71,7 +71,7 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 			books = append(books[:index], books[index+1:]...)
 			var book Book
 			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = strconv.Itoa(rand.Intn(10000000000)) // Mock id (not safe)
+			book.ID = params["id"]
 			books = append(books, book)
 
 			_ = json.NewEncoder(w).Encode(book)
