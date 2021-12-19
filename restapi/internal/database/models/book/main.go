14a22a36f99c2a67e0af985413fb394887cb2f48
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-19 20:44:15 +0330

    seeder updated

--- restapi/internal/database/models/book/main.go
+++ restapi/internal/database/models/book/main.go
@@ -7,6 +7,6 @@ type Book struct {
 	ID       int           `gorm:"primary_key" json:"id"`
 	Isbn     string        `json:"isbn"`
 	Title    string        `json:"title"`
-	AuthorID string        `json:"author_id"`
+	AuthorID int           `json:"author_id"`
 	Author   author.Author `gorm:"references:ID"`
 }
--- restapi/internal/database/seeder/main.go
+++ restapi/internal/database/seeder/main.go
@@ -3,21 +3,52 @@ package seeder
 import (
 	"gorm.io/gorm"
 	"math/rand"
+	"restapi/restapi/internal/database/models/author"
 	"restapi/restapi/internal/database/models/book"
 	"strconv"
 )
 
 // Seed : puts the initialized data into the database
 func Seed(db *gorm.DB) {
-	db.Create(&book.Book{
-		Isbn:   strconv.Itoa(rand.Int()),
-		Title:  "First book",
-		Author: "Mark Antony",
+	db.Create(&author.Author{
+		Firstname: "Roman",
+		Lastname:  "Petrovic",
+	})
+
+	db.Create(&author.Author{
+		Firstname: "Sandy",
+		Lastname:  "Jovic",
+	})
+
+	db.Create(&author.Author{
+		Firstname: "Peter",
+		Lastname:  "Lubernth",
+	})
+
+	db.Create(&author.Author{
+		Firstname: "Andy",
+		Lastname:  "Williams",
+	})
+
+	db.Create(&author.Author{
+		Firstname: "Dandy",
+		Lastname:  "Warhole",
+	})
+
+	db.Create(&author.Author{
+		Firstname: "Kevin",
+		Lastname:  "Ninto",
 	})
 
 	db.Create(&book.Book{
-		Isbn:   strconv.Itoa(rand.Int()),
-		Title:  "Second book",
-		Author: "Mary Morphy",
+		Isbn:     strconv.Itoa(rand.Int()),
+		Title:    "First book",
+		AuthorID: 1,
+	})
+
+	db.Create(&book.Book{
+		Isbn:     strconv.Itoa(rand.Int()),
+		Title:    "Second book",
+		AuthorID: 2,
 	})
 }
