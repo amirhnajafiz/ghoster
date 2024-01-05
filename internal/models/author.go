9103d40e4592cb366f358ddf76a019f87119c9a5
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2024-01-05 19:43:21 +0330

    Delete internal directory

--- internal/models/author.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// Author struct
-type Author struct {
-	ID        int `gorm:"primaryKey"`
-	Firstname string
-	Lastname  string
-}
--- internal/models/migrage.go
+++
@@ -1,8 +0,0 @@
-package models
-
-import "gorm.io/gorm"
-
-// Migrate : does the migrations of database
-func Migrate(db *gorm.DB) {
-	_ = db.Migrator().AutoMigrate(&Book{})
-}
