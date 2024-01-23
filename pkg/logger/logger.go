f950d18119dbd422f20290c436313b2f935692bf
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-23 14:12:23 +0330

    init: logger module

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,38 @@
 package logger
+
+type Logger interface {
+	Error(err error, params ...interface{})
+	Info(err error, params ...interface{})
+	Debug(err error, params ...interface{})
+	Trace(label string) Logger
+}
+
+func New() Logger {
+	return &logger{}
+}
+
+type logger struct {
+	tracer string
+}
+
+func (l logger) Error(err error, params ...interface{}) {
+
+}
+
+func (l logger) Info(err error, params ...interface{}) {
+
+}
+
+func (l logger) Debug(err error, params ...interface{}) {
+
+}
+
+func (l logger) Trace(label string) Logger {
+	if len(l.tracer) == 0 {
+		l.tracer = label
+	} else {
+		l.tracer = l.tracer + "." + label
+	}
+
+	return l
+}
