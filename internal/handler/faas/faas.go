801974ce98168675c283afbf3e663959ebca0e60
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-28 13:29:48 +0330

    init: handlers

--- internal/handler/faas/faas.go
+++ internal/handler/faas/faas.go
@@ -1,1 +1,10 @@
 package faas
+
+import "github.com/labstack/echo/v4"
+
+type Handler struct {
+}
+
+func (h Handler) Execute(ctx echo.Context) error {
+	return nil
+}
--- internal/handler/http/http.go
+++ internal/handler/http/http.go
@@ -1,3 +1,13 @@
 package http
 
+import "github.com/labstack/echo/v4"
+
 type HTTP struct{}
+
+func (h HTTP) Upload(ctx echo.Context) error {
+	return nil
+}
+
+func (h HTTP) Use(ctx echo.Context) error {
+	return nil
+}
