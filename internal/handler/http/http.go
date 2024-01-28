0c040b02d9ebdabdaf4a651e827a62ed02004938
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-28 18:07:58 +0330

    add: http upload handler

--- internal/handler/http/http.go
+++ internal/handler/http/http.go
@@ -1,11 +1,59 @@
 package http
 
-import "github.com/labstack/echo/v4"
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
 
 type HTTP struct{}
 
 func (h HTTP) Upload(ctx echo.Context) error {
-	return nil
+	// get file from form data
+	file, err := ctx.FormFile("project")
+	if err != nil {
+		log.Println(err)
+
+		return echo.ErrBadRequest
+	}
+
+	// check file extension
+	parts := strings.Split(file.Filename, ".")
+	if parts[len(parts)-1] != "zip" {
+		return echo.ErrBadRequest
+	}
+
+	// open file
+	src, err := file.Open()
+	if err != nil {
+		log.Println(err)
+
+		return echo.ErrInternalServerError
+	}
+
+	// create local file
+	dst, err := os.Create(file.Filename)
+	if err != nil {
+		log.Println(err)
+
+		return echo.ErrInternalServerError
+	}
+
+	// save content
+	if _, err = io.Copy(dst, src); err != nil {
+		log.Println(err)
+
+		return echo.ErrInternalServerError
+	}
+
+	src.Close()
+	dst.Close()
+
+	return ctx.NoContent(http.StatusOK)
 }
 
 func (h HTTP) Use(ctx echo.Context) error {
