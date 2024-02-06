7c46ad9e27fd4455700a0bcef2dd89cc8c52bd83
amirhnajafiz <najafizadeh21@gmail.com>
2024-02-06 09:43:21 +0330

    add: pipe termination and channel in pool

--- internal/agent/pool.go
+++ internal/agent/pool.go
@@ -3,22 +3,31 @@ package agent
 import "github.com/amirhnajafiz/ghoster/internal/agent/worker"
 
 type Pool struct {
+	Channel     chan string
+	Termination chan bool
+	Pipe        chan int
 }
 
-func NewPool(number int) (chan string, chan bool) {
+func NewPool(number int) Pool {
 	// create pool channels
 	channel := make(chan string)
 	termination := make(chan bool)
+	pipe := make(chan int)
 
 	// create workers
 	for i := 0; i < number; i++ {
 		w := worker.Worker{
 			Channel:   channel,
 			Terminate: termination,
+			Pipe:      pipe,
 		}
 
 		go w.Work()
 	}
 
-	return channel, termination
+	return Pool{
+		Channel:     channel,
+		Termination: termination,
+		Pipe:        pipe,
+	}
 }
