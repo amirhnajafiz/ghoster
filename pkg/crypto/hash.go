7895e0b63cfce8dbf605421731180acf3b66f134
amirhnajafiz <najafizadeh21@gmail.com>
2024-03-24 12:30:32 +0330

    add: md5 hashing

--- pkg/crypto/hash.go
+++ pkg/crypto/hash.go
@@ -1,1 +1,13 @@
 package crypto
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+// MD5Hash converts a string to its md5 hashing format.
+func MD5Hash(text string) string {
+	hash := md5.Sum([]byte(text))
+
+	return hex.EncodeToString(hash[:])
+}
