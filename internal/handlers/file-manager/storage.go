294175ba5bdd45f9cea745b9d9c3f30c08ab53fa
amirhnajafiz <najafizadeh21@gmail.com>
2024-03-27 17:51:52 +0330

    add: project utils files

--- internal/handlers/file-manager/storage.go
+++ internal/handlers/file-manager/storage.go
@@ -1,8 +1,13 @@
 package filemanager
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"time"
+
+	"github.com/amirhnajafiz/ghoster/pkg/crypto"
+	"github.com/amirhnajafiz/ghoster/pkg/model"
 )
 
 func createProjectDir(id string) error {
@@ -14,5 +19,26 @@ func createProjectDir(id string) error {
 }
 
 func createProjectMetaFile(path, name string) error {
+	stamp := time.Now()
 
+	meta := model.FileMeta{
+		Name:      name,
+		CreatedAt: stamp,
+		Hash:      crypto.MD5Hash(fmt.Sprint("%s-%t", name, stamp)),
+	}
+
+	bytes, err := json.Marshal(meta)
+	if err != nil {
+		return fmt.Errorf("failed to create file meta struct: %w", err)
+	}
+
+	file, err := os.Create(fmt.Sprintf("%s/data.meta.json", path))
+	if err != nil {
+		return fmt.Errorf("failed to create meta.json file: %w", err)
+	}
+
+	file.Write(bytes)
+	file.Close()
+
+	return nil
 }
