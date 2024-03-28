2012e6968509ce8cb40053f36e6730cd72baf6d1
amirhnajafiz <najafizadeh21@gmail.com>
2024-03-28 09:38:05 +0330

    add: projects templates

--- projects/separator/main.go
+++ projects/separator/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	if len(os.Args[1:]) < 2 {
+		panic("not enough arguments")
+	}
+
+	parts := strings.Split(os.Args[1], os.Args[2])
+
+	for _, part := range parts {
+		fmt.Println(part)
+	}
 }
--- projects/sumation/main.go
+++ projects/sumation/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
+func convertToIntOrPanic(input string) int {
+	tmp, err := strconv.Atoi(input)
+	if err != nil {
+		log.Println(err)
+
+		panic("input argument is not an integer type")
+	}
+
+	return tmp
+}
+
+func main() {
+	if len(os.Args[1:]) < 2 {
+		panic("not enough arguments")
+	}
+
+	a := convertToIntOrPanic(os.Args[1])
+	b := convertToIntOrPanic(os.Args[2])
+
+	fmt.Println(a + b)
 }
