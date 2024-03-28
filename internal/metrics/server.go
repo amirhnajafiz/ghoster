5e1e158e8377dd6a6769eb2275ebe5c9296e4a4e
amirhnajafiz <najafizadeh21@gmail.com>
2024-03-28 10:00:32 +0330

    add: prometheus server handler

--- internal/metrics/server.go
+++ internal/metrics/server.go
@@ -1,1 +1,15 @@
 package metrics
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func NewServer(port int) {
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	}()
+}
