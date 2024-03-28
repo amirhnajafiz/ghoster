aab443f3db6185c04c7accc97b850d76ee328aad
amirhnajafiz <najafizadeh21@gmail.com>
2024-03-28 09:53:30 +0330

    add: mux init in main

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/amirhnajafiz/ghoster
 
 go 1.22.0
 
-require github.com/google/uuid v1.6.0
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	internalHttp "github.com/amirhnajafiz/ghoster/internal/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// create a new mux router
+	router := mux.NewRouter()
+
+	// create an instance of internal handler
+	h := internalHttp.Handler{}
+
+	router.Methods(http.MethodGet).HandlerFunc(h.ListFunctions)
+
+	// create a new server
+	srv := &http.Server{
+		Handler: router,
+		Addr:    "127.0.0.1:5000",
+	}
+
+	// start the http server
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
