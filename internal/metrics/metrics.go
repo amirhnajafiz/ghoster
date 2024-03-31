817125aec87e3c8d0c0bb62cf3506483b17f509c
amirhnajafiz <najafizadeh21@gmail.com>
2024-03-31 10:30:48 +0330

    update: accessability of metrics

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -8,50 +8,50 @@ import (
 )
 
 type Metrics struct {
-	Requests             prometheus.CounterVec
-	FunctionCount        prometheus.CounterVec
-	FunctionFailure      prometheus.CounterVec
-	FunctionResponseTime prometheus.GaugeVec
+	requests             prometheus.CounterVec
+	functionCount        prometheus.CounterVec
+	functionFailure      prometheus.CounterVec
+	functionResponseTime prometheus.GaugeVec
 }
 
 func (m *Metrics) AddRequest(endpoint, method string) {
-	m.Requests.With(prometheus.Labels{"endpoint": endpoint, "method": method}).Add(1)
+	m.requests.With(prometheus.Labels{"endpoint": endpoint, "method": method}).Add(1)
 }
 
 func (m *Metrics) AddFunctionCount(functionName string, failed bool) {
 	if failed {
-		m.FunctionFailure.With(prometheus.Labels{"function": functionName}).Add(1)
+		m.functionFailure.With(prometheus.Labels{"function": functionName}).Add(1)
 	} else {
-		m.FunctionCount.With(prometheus.Labels{"function": functionName}).Add(1)
+		m.functionCount.With(prometheus.Labels{"function": functionName}).Add(1)
 	}
 }
 
 func (m *Metrics) AddFunctionResponseTime(functionName string, since time.Time) {
-	m.FunctionResponseTime.With(prometheus.Labels{"function": functionName}).Set(float64(time.Since(since) / 1000000))
+	m.functionResponseTime.With(prometheus.Labels{"function": functionName}).Set(float64(time.Since(since) / 1000000))
 }
 
 // Register metrics, creates prometheus metrics for ghoster.
 func Register(namespace, subsystem string) Metrics {
 	return Metrics{
-		Requests: *promauto.NewCounterVec(prometheus.CounterOpts{
+		requests: *promauto.NewCounterVec(prometheus.CounterOpts{
 			Name:      "total_requests",
 			Help:      "getting total number of requests per endpoint",
 			Namespace: namespace,
 			Subsystem: subsystem,
 		}, []string{"endpoint", "method"}),
-		FunctionCount: *promauto.NewCounterVec(prometheus.CounterOpts{
+		functionCount: *promauto.NewCounterVec(prometheus.CounterOpts{
 			Name:      "total_function_calls",
 			Help:      "getting total number of function calls",
 			Namespace: namespace,
 			Subsystem: subsystem,
 		}, []string{"function"}),
-		FunctionFailure: *promauto.NewCounterVec(prometheus.CounterOpts{
+		functionFailure: *promauto.NewCounterVec(prometheus.CounterOpts{
 			Name:      "total_function_failuers",
 			Help:      "getting total number of function failure calls",
 			Namespace: namespace,
 			Subsystem: subsystem,
 		}, []string{"function"}),
-		FunctionResponseTime: *promauto.NewGaugeVec(prometheus.GaugeOpts{
+		functionResponseTime: *promauto.NewGaugeVec(prometheus.GaugeOpts{
 			Name:      "function_response_time",
 			Help:      "response time of functions",
 			Namespace: namespace,
