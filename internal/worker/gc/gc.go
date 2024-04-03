df0476c6c65abdb0c373027d6c3ddb9ef95ee3eb
amirhnajafiz <najafizadeh21@gmail.com>
2024-04-03 09:30:44 +0330

    add: gc logic

--- internal/worker/gc/gc.go
+++ internal/worker/gc/gc.go
@@ -1,7 +1,25 @@
 package gc
 
-const functionsDir = "functions"
+import (
+	"log"
+	"time"
+)
 
-func NewGarbageCollector() {
+const (
+	functionsDir = "functions"
+	prefixToken  = "xxx-"
+)
 
+func NewGarbageCollector(interval int) {
+	tk := time.NewTicker(time.Duration(interval) * time.Second)
+
+	for {
+		<-tk.C
+
+		if count, err := deleteFilesWithStartToken(functionsDir, prefixToken); err != nil {
+			log.Printf("gc failed to run: %v\n", err)
+		} else if count > 0 {
+			log.Printf("gc collected %d items\n", count)
+		}
+	}
 }
